d741628ab8b76a9afa36d095ebb212dbeb10a943
claudiaarciniegas <claudiaaarciniegas83@gmail.com>
2021-04-29 13:44:19 -0500

    Actividad 1 de PHP 27-04-2021

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    //Declarar 5 variables con información personal
+
+    $nombre=(string)'Claudia Patricia Arciniegas Cáceres';
+    echo "Mi nombre es: $nombre";
+    echo '<br>';
+    $edad=(int)43;
+    echo "Mi edad es: $edad";
+    echo '<br>';
+    $direccion=(string)'Calle 193A # 31-27';
+    echo "Mi dirección es: $direccion";
+    echo '<br>';
+    const telefono=3183235415;
+    echo telefono;
+    echo '<br>';
+    $estatura='1.55';
+    $est=floatval($estatura);
+    echo "Mi estatura es: $est";
